f2dc13da9a7b60904e36144c09840d512c19a538
MAGerritsen <MA.Gerritsen@student.han.nl>
2022-11-14 23:22:08 +0100

    grammatica correctie

--- php/contact.php
+++ php/contact.php
@@ -7,7 +7,7 @@ if(isset($_POST['submit'])){
     $subject = "U heeft een nieuw bericht via martijngerritsen.online !";
     $subject2 = "Kopie van uw bericht op martijngerritsen.online";
     $message = $first_name . " " . $last_name . " heeft het volgende geschreven:" . "\n\n" . $_POST['message'];
-    $message2 = "Beste " . $first_name . ", u heeft onlangs een bericht achter gelaten op martijngerritsen.online." . "\n" . "Hier is een kopie van uw bericht:" . "\n\n" . $_POST['message'];
+    $message2 = "Beste " . $first_name . ", u heeft onlangs een bericht achtergelaten op martijngerritsen.online." . "\n" . "Hier is een kopie van uw bericht:" . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
