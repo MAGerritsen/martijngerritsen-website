96fadf45fbaf92716b1ef95a2ff4d9945ee0996b
MAGerritsen <MA.Gerritsen@student.han.nl>
2022-12-12 14:32:43 +0100

    Email-adres gewijzigd

--- php/contact.php
+++ php/contact.php
@@ -4,10 +4,10 @@ if(isset($_POST['submit'])){
     $from = $_POST['email'];
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
-    $subject = "U heeft een nieuw bericht via martijngerritsen.online !";
-    $subject2 = "Kopie van uw bericht op martijngerritsen.online";
+    $subject = "U heeft een nieuw bericht via martijngerritsen.com !";
+    $subject2 = "Kopie van uw bericht op martijngerritsen.com";
     $message = $first_name . " " . $last_name . " heeft het volgende geschreven:" . "\n\n" . $_POST['message'];
-    $message2 = "Beste " . $first_name . ", u heeft onlangs een bericht achtergelaten op martijngerritsen.online." . "\n" . "Hier is een kopie van uw bericht:" . "\n\n" . $_POST['message'];
+    $message2 = "Beste " . $first_name . ", u heeft onlangs een bericht achtergelaten op martijngerritsen.com." . "\n" . "Hier is een kopie van uw bericht:" . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
